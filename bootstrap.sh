3a02f24253964e47bc4f402075a4fd83312ef874
Cameron Furbush <Camfurbush@gmail.com>
2025-02-16 20:36:24 -0600

    push

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-
-# Setup Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval "$(/opt/homebrew/bin/brew shellenv)"
-brew bundle
-yadm clone git@github.com:Camfurbush/dotfiles.git
-
+# Determine if running on Linux or MacOS
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  echo "Running on Linux"
+  sudo apt update -y
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "Running on MacOS"
+  echo "Installing Homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+  brew bundle
+fi
 
 # Code Extensions
-
 code --install-extension 4ops.terraform
 code --install-extension DavidAnson.vscode-markdownlint
 code --install-extension eamodio.gitlens
@@ -19,3 +22,14 @@ code --install-extension redhat.vscode-yaml
 code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension Tyriar.sort-lines
 code --install-extension yzhang.markdown-all-in-one
+
+# Clone repos
+cd ~/development
+git clone git@github.com:camfu-co/hl_docker.git
+git clone git@github.com:camfu-co/hl_docker_images.git
+git clone git@github.com:camfu-co/hl_github_pipelines.git
+
+# Setup dotfiles
+yadm clone git@github.com:Camfurbush/dotfiles.git
+yadm reset --hard
+source ~/.bashrc
